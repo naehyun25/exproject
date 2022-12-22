e450614d01ea75aa337f053890ba4d48bc7607e7
naehyun25 <yoon658@naver.com>
2022-12-22 14:54:55 +0900

    NEW:반응형헤더구조작성

---
+++ js/script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#layer button").addEventListener("click", function () {
+    document.querySelector("#layer").style.display = "none";
+  });
+});
+
+
+
+
+// 문서의 요소(태그)가 다 준비되면 실행해 -. js 는 doncontentloaded jquery는 $(function)
+// $(값)<-제이쿼리 객체 <-제이쿼리에서 제공하는 함수를 실행할수있음
+// function함수를 객체로 만든거
+$(function () {
+  $(".full_menu, .nav li").mouseover(function () {
+    $(".full_menu").stop().slideDown(600);
+  });
+  $(".full_menu, .nav li").mouseleave(function () {
+    $(".full_menu").stop().slideUp(600);
+  });
+}); //jQuery
+$(function () {
+  $(".full_cover>ul>li").mouseover(function () {
+    var i=$(this).index();//원뎁스 li중 마우스 올라간요소의 번호를 i에 할당
+    $(".nav li").eq(i).find(".line").css("width,100%") //eq(n)n번째 요소를 취득
+//투뎁스 i번째에 마우스 오버시 원뎁스 i번째에 find()하위 탐색
+  });
+  $("full_cover>ul>li").mouseleave(function(){
+    var i=$(this).index();
+    $(".nav li").eq(i).find(".line").css("width","0%");
+  })
+  $(".nav li").mouseover(function(){
+    $(this).find(".line").css("width","100%");
+  })
+  $(".nav li").mouseleave(function(){
+    $(this).find(".line").css("width","0%");
+  })
+}); //jQuery
+//slideDown안에 트랜지션등 여러가지 애니메이션 값이 있어서 마우스를 여러번왔다갔다하면
+//   계속 조금씩 지연하면서 하기 때문에 계속 오르락내리락 한다
+//그래서 .stop() 함수를 써준다-> 지금 지시받은 일이 있으면 멈추고 그것부터해
+//제이쿼리든 함수든 태그는 문자열로 인식해서 ""나 ''안에써준다.
+
+//햄버거메뉴 
+$(function(){
+$("#header .mo").click(function(){
+  $(".mo_menu").stop().slideToggle("fast");
+  $("#header. mo").css('color','red');
+})
+});
