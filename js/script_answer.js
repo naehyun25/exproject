9b3d5322939975710796f113f05a13cdfb95eb2f
naehyun25 <yoon658@naver.com>
2023-01-16 15:16:11 +0900

    슬라이드 수정 변수 및 함수정리

--- js/script_answer.js
+++ js/script_answer.js
@@ -1,205 +1,230 @@
-document.addEventListener("DOMContentLoaded", function () {
-	document.querySelector("#layer button").addEventListener("click", function () {
-		document.querySelector("#layer").style.display = "none";
-	});
-});
-/* 문서의 요소(태그)가 다 준비되면 실행해 */
-/* $(값) <- 제이쿼리 객체 <- 제이쿼리 함수를 실행할수 있음  */
 $(function () {
-  gnb();
-  function gnb(){
-	$(".full_menu, .nav li").mouseover(function () {
-		$(".full_menu").stop().slideDown(600);
-	});
-	$(".full_menu, .nav li").mouseleave(function () {
-		$(".full_menu").stop().slideUp(600);
-	});
-	$(".full_cover>ul>li").mouseover(function () {
-		var i = $(this).index();
-		$(".nav li").eq(i).find(".line").css("width", "100%");
-	});
-	$(".full_cover>ul>li").mouseleave(function () {
-		var i = $(this).index();
-		$(".nav li").eq(i).find(".line").css("width", "0%");
-	});
-	$(".nav li").mouseover(function () {
-		$(this).find(".line").css("width", "100%");
-	});
-	$(".nav li").mouseleave(function () {
-		$(this).find(".line").css("width", "0%");
-	});
-	$("#header .mo").click(function () {
-		$(".mo_menu").stop().slideToggle("fast");
-		$("#header .mo").css("color", "red");
-	});
-};
+	gnb();
+	function gnb() {
+		$(".full_menu, .nav li").mouseover(function () {
+			$(".full_menu").stop().slideDown(600);
+		});
+		$(".full_menu, .nav li").mouseleave(function () {
+			$(".full_menu").stop().slideUp(600);
+		});
+		$(".full_cover>ul>li").mouseover(function () {
+			var i = $(this).index();
+			$(".nav li").eq(i).find(".line").css("width", "100%");
+		});
+		$(".full_cover>ul>li").mouseleave(function () {
+			var i = $(this).index();
+			$(".nav li").eq(i).find(".line").css("width", "0%");
+		});
+		$(".nav li").mouseover(function () {
+			$(this).find(".line").css("width", "100%");
+		});
+		$(".nav li").mouseleave(function () {
+			$(this).find(".line").css("width", "0%");
+		});
+		$("#header .mo").click(function () {
+			$(".mo_menu").stop().slideToggle("fast");
+			$("#header .mo").css("color", "red");
+		});
+	}
+
+	var current = 0; //current
+	var prev = null; //prev
+	var next = null; //next
+	var mainInterval; //setIntervalID
+	var speed = 2000; //반복시간
+	var pause_css = $(".pause_css").css("display");
+	var slides = $(".slide_cover ul li");
+	var obj = {
+		btn: $(".slide_btn ul li"),
+		btn_ani: $(".color"),
+		left: $(".cursor_left"),
+		right: $(".cursor_right"),
+		wbox: $(".white_box"),
+		cbox: $(".box"),
+		logo: $(".img_logo"),
+		text: $(".img_text"),
+		reset: $(".reset"),
+		pause: $(".pause_btn"),
+		play: $(".play_btn"),
+		remove: function (x) {
+			x.removeClass("on");
+		},
+		add: function (x) {
+			x.addClass("on");
+		},
+	};
+
 	/**
-	 * mainCarousel
-	 * 시간마다 일시키는 timer
-	 * 애니메이션효과를 갖고있는 slide
-   * 시간마다 슬라이드 계산함수
-   * -> 시간마다 인덱스 계산 , slide함수 호출함
+	 * 시간마다 인덱스 계산, slide함수 호출함
 	 * * */
-	var i = 0 // current
-		var k = null; // k는 prev
-    var n= null; // next
-    var mainInterval = null; //current
-    var speed =2000; //반복시간
-    var mainInterval;// setintervalid
-	timer();
+	//timer();
 	function timer() {
-		mainInterval=setInterval(function () {
-			i++;
-			k = i - 1;
-			if (i == 3) {
-				i = 0;
+		mainInterval = setInterval(function () {
+			current++;
+			prev = current - 1;
+			if (current == 3) {
+				current = 0;
 			}
 			slide();
 		}, speed);
 	}
 
 	function slide() {
-		$('.slide_btn ul li').off('click');
-		$('.cursor_left, .cursor_right').off('click');
-		$('.reset').off('click',reset);//비동기방식이라리셋이란 함수를 호출함
+		obj.btn.off("click");
+		obj.left.off("click");
+		obj.right.off("click");
+		obj.reset.off("click", reset);
 		clearInterval(mainInterval);
-    //-100% -> 0 
-		$(".slide_cover ul li").find(".white_box")
-    .css("left", "-100%").stop()
-    .animate({ left: "0%" }, 1000,
-    function(){
-      $(".white_box").stop().animate({left:"100%"},1000)
-      $(".slide_cover ul li").eq(i).addClass("on");
-      $(".slide_cover ul li").eq(k).removeClass("on");
-			$(".color, .img_logo, .img_text, .box").removeClass('on');
-      $(".slide_btn ul li").eq(i).find(".color").addClass("on");
-      $(".slide_cover ul li").eq(i).find(".img_logo").addClass("on");
-      $(".slide_cover ul li").eq(i).find(".img_text").addClass("on");
-      $(".slide_cover ul li").eq(i).find(".box").addClass("on");
-      $(".slide_cover ul li a img").removeClass("on");		
-      $(".slide_cover ul li").eq(i).find("a img").addClass("on");
-    });
-    
-    $(".slide_cover ul li")
-    .eq(k)
-    .animate({left:0}, speed,function(){
-      setTimeout(function(){//지정시간지나고딱한번만하는일
-      //trigger
-			$('.slide_cover ul li').on("click",click_slide);
-			$('.cursor_left').on("click",left_click);
-			$('.cursor_right').on("click",right_click)
-      },1000)
-			var pause_css=$('.pause_css').css("display")
-			if(pause_css=="none"){clearInterval(mainInterval);
-			$(".color").removeClass("on");}
-			else{
+
+		slides
+			.find(obj.wbox).css("left", "-100%").stop().animate({ left: "0%" }, 1000, function () {obj.wbox.stop().animate({ left: "100%" }, 1000);
+			/*obj라는 객체에 
+			* obj.remove()
+			* obj.add()
+			*라는 메서드를 만들어놓음
+			*/ 
+				obj.add(slides.eq(current));
+				obj.remove(slides.eq(prev)); // 대상이 뒤에 ()들어가야함
+				
+				//$(".color, .img_logo, .img_text, .box").removeClass("on");->아래처럼 표현됨
+				obj.remove(obj.btn_ani)
+				obj.remove(obj.text)
+				obj.remove(obj.logo)
+				obj.remove(obj.cbox)//모든 애니메이트요소 삭제
+				
+				//$(".slide_btn ul li").eq(current).find(".color").addClass("on");->아래처럼 표현됨
+				//obj.add($(".slide_btn ul li").eq(current).find(".color"))->한번더 객체화시키면 아래처럼됨
+				obj.add(obj.btn.eq(current).find(obj.btn_ani));
+				
+
+				//slides.eq(current).find(".img_logo").addClass("on");-1
+				//obj.add(slides.eq(current).find(".img_logo"))-2
+				obj.add(slides.eq(current).find(obj.logo))//-3
+				
+				obj.add(slides.eq(current).find(obj.text))
+				obj.add(slides.eq(current).find(obj.cbox))
+				obj.remove(slides.find("a img"))
+				obj.add(slides.eq(current).find("a img"))
+			});
+					//1초후 한번 	클릭이벤트 실행
+		setTimeout(function () {
+			//trigger
+			slides.on("click", click_slide);
+			obj.left.on("click", left_click);
+			obj.right.on("click", right_click);
+
+			if (pause_css == "none") {
+				clearInterval(mainInterval);
+				obj.remove(obj.btn_ani)
+				//$(".color").removeClass("on");
+			} else {
 				timer();
-				$('.slide_btn ul li').eq(i).find(".color").addClass('on')
+				obj.add(obj.btn.eq(current).find(obj.btn_ani))
 			}
-
-    })
-		n=i;
-	}
-/**
- * click_slide
- * pager 함수
- */
-function click_slide(){
-	i=$(this).index();
-	if(i==n)return;
-	k=n;
-	slide();
-	$(".color").removeClass.on
-}
-
-  //click_slide(페이져버튼)
-
-  /**
-   * left_click
-   * left 커서함수
-   */
-	function left_click(){
-		i--;
-	 	if(i==-1)i=2
-		if(i==n) return;
-		k=n ;
-		slide();
-		$('.color')
-		.removeClass('on')
-	}
-  /**
-   * right_click
-   * right 커서함수
-   */
-	function right_click(){
-	 i++;
-	 if(i==3)i=0
-	if(i==n) return;
-	k=n ;
-	slide();
-	$('.color')
-	.removeClass('on');
+		}, 1000);
+		next = current;//1
 	}
 
-  /**
-   * reset
-   * play,stop
-   */
-	function reset(){
-		var pause_css=$('.pause_btn').css('display')
-		var start_css=$('.start_btn').css('display')
-		if(pause_css=="none"){
-			clearInterval(mainInterval);
-		 $('.color').removeClass('on')
-		}else{
-			timer();
-			$('.slide_btn ul li').eq(i).find('.color').addClass('on')
-		}
-	}
 	/**
-	 * 이벤트 핸들러
-	 */
-	$('.reset').click(function(){
-		reset();
-	})
-	$('.reset .pause_btn').click(function(){
-		$('.pause_btn').css('display','none');
-		$('.start_btn').css('display','block'); 
-	})
-	$('.reset .start_btn').click(function(){
-		$('.pause_btn').css('display','block');
-		$('.start_btn').css('display','none'); 
-	})
+	 * click_slide
+	 * pager 함수
+	 *
+	 * **/
+	function click_slide() {
+		current = $(this).index();
+		if (current == next) return;
+		prev = next;
+		slide();
+		obj.remove(obj.btn_ani)
+	}
 
-	 $('.slide_btn ul li').on('click',click_slide)
-	$('.cursor_left').on('click',left_click)
-	$('.cursor_right').on('click',right_click)
-	$('.main_cursor').mousemove(function(e){
-		var x=e.clientX;
-		var y=e.clientY;
-		$('.cursor').css({left:x,top:y})
-	})
+	/**
+	 * left_click
+	 * left 커서함수
+	 *
+	 * **/
+	function left_click() {
+		current--;
+		if (current == -1) current = 2;
+		if (current == next) return;
+		prev = next;
+		slide();
+		obj.remove(obj.btn_ani)
+	
+	}
 
-/** 
-* resize
-**/
+	/**
+	 * right_click
+	 * right 커서함수
+	 *
+	 * ***/
+	function right_click() {
+		current++;
+		if (current == 3) current = 0;
+		if (current == next) return;
+		prev = next;
+		slide();
+		obj.remove(obj.btn_ani)
+	}
 
-$(window).resize(function(){
-	var w_width=$(window).width();
-	var img_height=$(".slide_cover ul li img.pc").height();
-	var img_height_mo=$(".slide_cover ul li img.mo").height();
-	if(w_width>1330){
-		$("#main_slide .slide_cover").height(img_height)
-	}else{
-		$("#main_slide .slide_cover").height(img_height_mo);
-		$(".slide_cover ul li img.pc").css("display","none");
-		$(".slide_cover ul li img.mo").css("display","block");
+	/**
+	 * reset
+	 * play,stop
+	 *
+	 * ***/
+	/* 비동기 함수 */
+	function reset() {
+		if (pause_css == "none") {
+			clearInterval(mainInterval);
+			obj.remove(obj.btn_ani)
+		} else {
+			timer();
+			obj.add(obj.btn.eq(current).find(obj.btn_ani))
 		}
-})
-$(window).trigger('resize');
+	}
 
+	/**이벤트 핸들러 */
+	obj.reset.click(function () {
+		reset();
+	});
+	obj.pause.click(function () {
+		obj.pause.css("display", "none");
+		obj.play.css("display", "block");
+	});
+	obj.play.click(function () {
+		obj.pause.css("display", "block");
+		obj.play.css("display", "none");
+	});
+
+	obj.btn.on("click", click_slide);
+	obj.left.on("click", left_click);
+	obj.right.on("click", right_click);
+
+	$(".main_cursor").mousemove(function (e) {
+		var x = e.clientX;
+		var y = e.clientY - 80;
+		$(".cursor").css({
+			left: x,
+			top: y,
+		});
+	});
+
+	/***
+	 * Resize
+	 * * */
+
+	$(window).resize(function () {
+		var w_width = $(window).width();
+		var img_height = $(".slide_cover ul li img.pc").height();
+		var img_height_mo = $(".slide_cover ul li img.mo").height();
+		if (w_width > 1330) {
+			$("#main_slide .slide_cover,#main_slide").height(img_height);
+		} else {
+			$("#main_slide .slide_cover,#main_slide").height(img_height_mo);
+			$(".slide_cover ul li img.pc").css("display", "none");
+			$(".slide_cover ul li img.mo").css("display", "block");
+		}
+	});
+
+	$(window).trigger("resize");
 }); //jQuery
-
-
-
-
